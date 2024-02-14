96dd66cdfc642cafecfe3e66b33e87ae7f1212dc
kknowk <horike.kaisyu@gmail.com>
2024-02-14 15:00:32 +0900

    add SDGs

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Privacy from './Privacy';
 import Products from './Products';
 import InstagramStats from './components/InstagramStats'; // 新しいページコンポーネントをインポート
 import CreatorInfo from './components/CreatorInfo'; // コンポーネントをインポート
+import SDGsInfo from './components/SDGsInfo'; // コンポーネントをインポート
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faLine } from '@fortawesome/free-brands-svg-icons';
 
@@ -27,6 +28,7 @@ function App() {
                 bio="ニューワールドのオーナーシェフ. sio株式会社 / シズる株式会社 総料理責任者。"
                 imageUrl="https://github.com/kknowk/kknowk/assets/131352588/2a5f6c0f-e6dc-4d58-9dca-fbda5d33cfe6"
               />
+              <SDGsInfo /> 
               <div style={{ marginTop: '20px', textAlign: 'center', border: '1px solid', padding: '20px', fontSize: '1.2em' }}>
                 <p style={{ fontSize: '2em', marginBottom: '20px' }}>公式SNS</p>
                 <a href="https://www.instagram.com/futsunoshop/" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
--- src/components/SDGsInfo.tsx
+++ src/components/SDGsInfo.tsx
@@ -1,33 +1,27 @@
-// src/components/CreatorInfo.tsx
+// src/components/SDGsInfo.tsx
 
 import React from 'react';
 import { Card, CardMedia, CardContent, Typography } from '@mui/material';
 
-interface CreatorInfoProps {
-  name: string;
-  bio: string;
-  imageUrl: string;
-}
-
-const CreatorInfo: React.FC<CreatorInfoProps> = ({ name, bio, imageUrl }) => {
+const SDGsInfo: React.FC = () => {
   return (
     <Card sx={{ maxWidth: 345, margin: 'auto', marginTop: '30px' }}>
       <CardMedia
         component="img"
-        height="350"
-        image={imageUrl}
-        alt={name}
+        height="330"
+        image="https://github.com/kknowk/kknowk/assets/131352588/419f903e-a45d-4d4a-b9af-4058f3b51c0e"
+        alt="SDGs Goal"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {name}
+          SDGsの3番
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          {bio}
+        あらゆる年齢のすべての人々の健康的な生活を確保し、福祉を促進する。
         </Typography>
       </CardContent>
     </Card>
   );
 };
 
-export default CreatorInfo;
+export default SDGsInfo;
