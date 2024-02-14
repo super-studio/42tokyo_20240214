3f432b1df6041e00609f1c44a0dcec2ea7830966
kknowk <horike.kaisyu@gmail.com>
2024-02-14 14:38:36 +0900

    add 監修

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Privacy from './Privacy';
 import Products from './Products';
 import InstagramStats from './components/InstagramStats'; // 新しいページコンポーネントをインポート
+import CreatorInfo from './components/CreatorInfo'; // コンポーネントをインポート
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faLine } from '@fortawesome/free-brands-svg-icons';
 
@@ -21,6 +22,11 @@ function App() {
           <Route path="/" element={
             <>
               <Products />
+              <CreatorInfo
+                name="[監修] 鳥羽 周作"
+                bio="ニューワールドのオーナーシェフ. sio株式会社 / シズる株式会社 総料理責任者。"
+                imageUrl="https://github.com/kknowk/kknowk/assets/131352588/2a5f6c0f-e6dc-4d58-9dca-fbda5d33cfe6"
+              />
               <div style={{ marginTop: '20px', textAlign: 'center', border: '1px solid', padding: '20px', fontSize: '1.2em' }}>
                 <p style={{ fontSize: '2em', marginBottom: '20px' }}>公式SNS</p>
                 <a href="https://www.instagram.com/futsunoshop/" target="_blank" rel="noopener noreferrer" style={{ margin: '0 10px' }}>
